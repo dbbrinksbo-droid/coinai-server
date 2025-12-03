8192bfe9b77f14d1e47e254e778b875c9a166d8a
dbbrinksbo-droid <dbbrinksbo@gmail.com>
2025-12-03 19:32:33 +0100

    Update metadata_builder.py

--- modules/metadata_builder.py
+++ modules/metadata_builder.py
@@ -1,15 +1,10 @@
 import time
 
-def build_metadata(prediction, ocr_text=None, gpt_notes=None):
-    """
-    Samler ALLE informationer om mønten ét sted.
-    Bruges af full_analyze.
-    """
-
+def build_metadata(prediction, ocr_text, gpt_notes):
     return {
         "timestamp": int(time.time()),
-        "model_prediction": prediction,         # fra ONNX
-        "ocr_text": ocr_text,                  # årstal / tekst på mønt
-        "gpt_enhancement": gpt_notes,          # ekstra analyser fra GPT
+        "model_prediction": prediction,
+        "ocr_text": ocr_text,
+        "gpt_enhancement": gpt_notes,
         "status": "ok"
     }
